b3616027ab71cf1eccd4457a646b153c5cb97626
ydh960823 <ydh960823@hakjisa.co.kr>
2025-06-17 08:55:02 +0900

    접속자수 추가

--- server.js
+++ server.js
@@ -10,9 +10,15 @@ const io = new Server(server, {
     origin: "*", // 모든 프론트엔드에서 접속 허용 (개발용)
   },
 });
+let connectedUsers = 0;
 
 io.on("connection", (socket) => {
+  connectedUsers++;
   console.log("🔌 유저 접속됨:", socket.id);
+  console.log("📊 현재 접속자 수:", connectedUsers);
+
+  // 접속자 수를 모든 클라이언트에 전송
+  io.emit("user count", connectedUsers);
 
   socket.on("chat message", (msg) => {
     console.log("📩 받은 메시지:", msg);
@@ -20,7 +26,12 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
+    connectedUsers--;
     console.log("❌ 유저 연결 해제:", socket.id);
+    console.log("📊 현재 접속자 수:", connectedUsers);
+
+    // 접속자 수 업데이트
+    io.emit("user count", connectedUsers);
   });
 });
 
