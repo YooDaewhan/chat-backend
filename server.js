0ba9bdd3ec740407abf72a2496b9ea7b337d96d9
ydh960823 <ydh960823@hakjisa.co.kr>
2025-06-26 16:47:58 +0900

    mid 추가

--- server.js
+++ server.js
@@ -224,7 +224,7 @@ app.post("/api/login", async (req, res) => {
 
 app.get("/api/monsters/all", async (req, res) => {
   try {
-    const [rows] = await pool.query("SELECT uid, name FROM monster");
+    const [rows] = await pool.query("SELECT uid, name, mid FROM monster");
     res.json(rows);
   } catch (err) {
     console.error("전체 몬스터 조회 실패:", err);
